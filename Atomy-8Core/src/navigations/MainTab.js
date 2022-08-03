c2ec989b15084dc4d7980df56f8ab9554335ba06
UniM0cha <solst_ice@naver.com>
2022-08-04 01:24:18 +0900

    0804 - 안드로이드 키보드 위로 탭바 나오는 문제 해결 / ios, android 폴더 gitignore에 추가

--- Atomy-8Core/src/navigations/MainTab.js
+++ Atomy-8Core/src/navigations/MainTab.js
@@ -11,6 +11,7 @@ const TabNavigation = () => {
     <Tab.Navigator
       initialRouteName="Main"
       barStyle={{ backgroundColor: 'white' }}
+      screenOptions={{ tabBarHideOnKeyboard: true }}
     >
       <Tab.Screen
         name="Main"
