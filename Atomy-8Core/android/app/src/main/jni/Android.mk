24703890e09b0133ece6b7d02d6ed3f9c420eae6
UniM0cha <solst_ice@naver.com>
2022-08-04 19:47:12 +0900

    0804 - 아이폰 앱 빌드 & 배포

--- Atomy-8Core/android/app/src/main/jni/Android.mk
+++ Atomy-8Core/android/app/src/main/jni/Android.mk
@@ -10,7 +10,7 @@ include $(CLEAR_VARS)
 LOCAL_PATH := $(THIS_DIR)
 
 # You can customize the name of your application .so file here.
-LOCAL_MODULE := 8_appmodules
+LOCAL_MODULE := 8core_appmodules
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/*.cpp)
